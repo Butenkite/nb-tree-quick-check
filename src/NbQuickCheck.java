c620479af63656a3352f57df7bb6e183e444e3b9
unknown <174657090+Butenkite@users.noreply.github.com>
2025-05-21 16:29:57 -0700

    all passing

--- src/NbQuickCheck.java
+++ src/NbQuickCheck.java
@@ -11,8 +11,10 @@ public class NbQuickCheck {
    * @param root the root node to start traversal from
    */
   public static void preOrder(Map<Integer, List<Integer>> tree, int root) {
-    if(!tree.containsKey(root)) {
-      return;
+    if(!tree.containsKey(root)) return;
+    System.out.println(root);
+    for(int child : tree.get(root)){
+      preOrder(tree, child);
     }
   }
 
@@ -24,7 +26,14 @@ public class NbQuickCheck {
    * @return the minimum value in the tree or Integer.MAX_VALUE if root is null
    */
   public static int minVal(Node<Integer> root) {
-    return -1;
+    if(root == null) return Integer.MAX_VALUE;
+    int lowest = root.value;
+    for (var child : root.children) {
+      int lower = minVal(child);
+      if (lower < lowest) {
+        lowest = lower;
+      }
+    }
+    return lowest;
   }
-  
 }
